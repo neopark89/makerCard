2b48a6ba4254debe0c7639dbc35d265d1447221b
neopark <neopark89@gmail.com>
2022-02-05 23:30:00 +0900

    add login & logout function

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styles from "./app.module.css";
 import Login from "./components/login/login";
+import Maker from './components/maker/maker';
 
 function App({ authService }) {
   return (
-    <p className={styles.app}>
-      <Login authService={authService} />
-    </p>
+    <div className={styles.app}>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login authService={authService} />} />
+          <Route path="/maker" element={<Maker authService={authService} />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Footer from "../footer/footer";
 import Header from "../header/header";
-import styles from './login.module.css';
+import styles from "./login.module.css";
 
 const Login = ({ authService }) => {
+  const navigate = useNavigate();
+
+  const goToMaker = (userId) => {
+    navigate({
+      pathname: '/maker',
+      state: { id: userId }, 
+    });
+  };
+
   const onLogin = (event) => {
     authService //
-    .login(event.currentTarget.textContent)
-    .then(console.log);
+      .login(event.currentTarget.textContent)
+      .then(data => goToMaker(data.user.uid));
   };
+
+  useEffect (() => {
+    authService.onAuthChange(user => {
+      user && goToMaker(user.uid)
+    });
+  })
 
   return (
     <section>
@@ -17,10 +33,14 @@ const Login = ({ authService }) => {
         <h1>Login</h1>
         <ul className={styles.list}>
           <li className={styles.item}>
-            <button className={styles.button} onClick={onLogin} >Google</button>
+            <button className={styles.button} onClick={onLogin}>
+              Google
+            </button>
           </li>
           <li className={styles.item}>
-            <button className={styles.button} onClick={onLogin} >Github</button>
+            <button className={styles.button} onClick={onLogin}>
+              Github
+            </button>
           </li>
         </ul>
       </section>
