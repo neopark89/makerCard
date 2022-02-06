ab07e9af23df3ff020a106c2c60dc999d4b60366
neopark <neopark89@gmail.com>
2022-02-06 18:28:44 +0900

    make card edit list

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Editor from "../editor/editor";
 import Footer from "../footer/footer";
@@ -7,6 +7,43 @@ import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 
 const Maker = ({ authService }) => {
+
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Neo',
+      company: 'Nudgecode',
+      theme: 'dark',
+      title: 'ceo',
+      email: 'neoesse@nudgecode.com',
+      message: 'go for it',
+      fileName: 'neo',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'jenny',
+      company: 'Nudgecode',
+      theme: 'light',
+      title: 'designer',
+      email: 'jenny@nudgecode.com',
+      message: 'go for it',
+      fileName: 'neo',
+      fileURL: 'neo.png',
+    },
+    {
+      id: '3',
+      name: 'Bora',
+      company: 'Nudgecode',
+      theme: 'colorful',
+      title: 'developer',
+      email: 'bora@nudgecode.com',
+      message: 'go for it',
+      fileName: 'neo',
+      fileURL: null,
+    }
+  ]);
+
   const navigate = useNavigate();
 
   const onLogout = () => {
@@ -25,8 +62,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
