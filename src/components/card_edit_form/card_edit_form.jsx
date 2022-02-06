9f02187485a3d597ce93c906420fd97b66579ef1
neopark <neopark89@gmail.com>
2022-02-06 20:40:24 +0900

    add CardAddForm component

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -23,9 +23,9 @@ const CardEditForm = ({ card }) => {
         value={company}
       ></input>
       <select className={styles.select} name="theme" value={theme}>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-        <option value="colorful">Colorful</option>
+        <option value="light">light</option>
+        <option value="dark">dark</option>
+        <option value="colorful">colorful</option>
       </select>
       <input
         className={styles.input}
