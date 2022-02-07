051b1e62bd5975d47e5b99e662f550497fab5e9e
neopark <neopark89@gmail.com>
2022-02-07 23:12:33 +0900

    image uploader first

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
-import styles from './image_file_input.module.css';
+import React, { useRef } from "react";
+import styles from "./image_file_input.module.css";
 
-const ImageFileInput = (props) => (
-    <button>image</button>  
+const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const inputRef = useRef();
+
+  const onButtonClick = () => {
+    inputRef.current.click();
+  };
+
+  const onChange = () => {
+
+  }
+
+  return (
+    <div className={styles.container}>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="file"
+        accept="image/*"
+        name="file"
+        onChange={onChange}
+      />
+      <button className={styles.button} onClick={onButtonClick}>
+        {name || "No file"}
+      </button>
+    </div>
   );
+};
 
 export default ImageFileInput;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -6,7 +6,7 @@ import Header from "../header/header";
 import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 
-const Maker = ({ authService }) => {
+const Maker = ({ FileInput, authService }) => {
 
   const [cards, setCards] = useState({
     '1': {
@@ -64,7 +64,7 @@ const Maker = ({ authService }) => {
       updated[card.id] = card;
       return updated;
     })
-        
+      
   }
 
   const deleteCard = (card) => {
@@ -79,7 +79,7 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard} />
+        <Editor FileInput={FileInput} cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard} />
         <Preview cards={cards} />
       </div>
       <Footer />
